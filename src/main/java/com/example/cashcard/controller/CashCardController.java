1abbf53e7e13371d4b19032153613a7a821bd787
DanielZheng916 <113737591+DanielZheng916@users.noreply.github.com>
2023-07-09 20:52:22 -0400

    Update CashCardController findById method to return 404 error when card is not found

--- src/main/java/com/example/cashcard/controller/CashCardController.java
+++ src/main/java/com/example/cashcard/controller/CashCardController.java
@@ -2,22 +2,31 @@ package com.example.cashcard.controller;
 
 import com.example.cashcard.dto.CashCardResponse;
 import com.example.cashcard.model.CashCard;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/cashcards")
+@Slf4j
 public class CashCardController {
 
     @GetMapping("/{id}")
-    public CashCardResponse findById() {
-        CashCard newCashCard = new CashCard(1000L, 0.0);
-        CashCardResponse cashCardResponse = CashCardResponse.builder()
-                .id(newCashCard.getId())
-                .amount(newCashCard.getAmount())
-                .build();
-        return cashCardResponse;
+    public CashCardResponse findById(@PathVariable Long id) {
+        if (id.equals(99L)) {
+            CashCard newCashCard = new CashCard(99L, 123.45);
+            CashCardResponse cashCardResponse = CashCardResponse.builder()
+                    .id(newCashCard.getId())
+                    .amount(newCashCard.getAmount())
+                    .build();
+            return cashCardResponse;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cash card not found");
+        }
     }
 }
